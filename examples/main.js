95cb9f90f0127f727475a1256a8784450e016f03
bjslts <1801815529@qq.com>
2023-05-25 01:09:31 +0800

    高亮代码 组件渲染

--- examples/main.js
+++ examples/main.js
@@ -6,6 +6,21 @@ import MeUI from '../src/index'
 
 Vue.use(MeUI)
 
+import hljs from 'highlight.js'
+import demoBlock from './components/demo-block'
+import 'highlight.js/styles/stackoverflow-light.css'
+
+Vue.component('demo-block', demoBlock)
+
+router.afterEach(() => {
+  // https://github.com/highlightjs/highlight.js/issues/909#issuecomment-131686186
+  Vue.nextTick(() => {
+    const blocks = document.querySelectorAll('pre code:not(.hljs)')
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+  })
+  document.title = 'me-ui'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
